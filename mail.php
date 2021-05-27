834fc22c5fa975b7e42328b133da8d006fee8f7d
Hamza Mufti <hamza101muf@gmail.com>
2021-05-27 06:51:47 -0400

    Images optimized for loading times + color scheme slider

--- mail.php
+++ mail.php
@@ -4,24 +4,32 @@ $phone = trim($_POST['contact-phone']);
 $email = trim($_POST['contact-email']);
 $message = trim($_POST['contact-message']);
 if ($name == "") {
-    $msg['err'] = "\n Need a name, pls.";
+    $msg['err'] = "\n Name can not be empty!";
     $msg['field'] = "contact-name";
     $msg['code'] = FALSE;
+} else if ($phone == "") {
+    $msg['err'] = "\n Phone number can not be empty!";
+    $msg['field'] = "contact-phone";
+    $msg['code'] = FALSE;
+} else if (!preg_match("/^[0-9 \\-\\+]{4,17}$/i", trim($phone))) {
+    $msg['err'] = "\n Please put a valid phone number!";
+    $msg['field'] = "contact-phone";
+    $msg['code'] = FALSE;
 } else if ($email == "") {
-    $msg['err'] = "\n Seems like there's no email?";
+    $msg['err'] = "\n Email can not be empty!";
     $msg['field'] = "contact-email";
     $msg['code'] = FALSE;
 } else if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
-    $msg['err'] = "\n I can't understand your email address";
+    $msg['err'] = "\n Please put a valid email address!";
     $msg['field'] = "contact-email";
     $msg['code'] = FALSE;
 } else if ($message == "") {
-    $msg['err'] = "\n The message box is for writing messages";
+    $msg['err'] = "\n Message can not be empty!";
     $msg['field'] = "contact-message";
     $msg['code'] = FALSE;
 } else {
     $to = 'hamza101muf@gmail.com';
-    $subject = '👋 So...';
+    $subject = 'So...';
     $_message = '<html><head></head><body>';
     $_message .= '<p>Name: ' . $name . '</p>';
     $_message .= '<p>Message: ' . $phone . '</p>';
