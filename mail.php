982984da126ee1cd749e93798119a12288c3246e
Hamza Mufti <hamza101muf@gmail.com>
2021-06-10 17:15:31 -0400

    updates fav

--- mail.php
+++ mail.php
@@ -1,10 +1,15 @@
 <?php
 $name = trim($_POST['contact-name']);
+$phone = trim($_POST['contact-phone']);
 $email = trim($_POST['contact-email']);
 $message = trim($_POST['contact-message']);
 if ($name == "") {
     $msg['err'] = "\n Name can not be empty!";
     $msg['field'] = "contact-name";
+    $msg['code'] = FALSE;
+} else if (!preg_match("/^[0-9 \\-\\+]{4,17}$/i", trim($phone))) {
+    $msg['err'] = "\n Please put a valid phone number!";
+    $msg['field'] = "contact-phone";
     $msg['code'] = FALSE;
 } else if ($email == "") {
     $msg['err'] = "\n Email can not be empty!";
@@ -20,7 +25,7 @@ if ($name == "") {
     $msg['code'] = FALSE;
 } else {
     $to = 'hamza101muf@gmail.com';
-    $subject = 'So...';
+    $subject = 'Hey 👋';
     $_message = '<html><head></head><body>';
     $_message .= '<p>Name: ' . $name . '</p>';
     $_message .= '<p>Email: ' . $email . '</p>';
